3928ff7eacd7e132a41a98cc5503ead6ba058d9c
abdelrhmanali29 <abdelrahman.ali.sayed28@gmail.com>
2020-11-23 01:24:14 +0200

    change backend URI

--- src/Match.js
+++ src/Match.js
@@ -1,10 +1,17 @@
 import React from "react";
-import Axios from 'axios';
-import {useState, useEffect} from "react"
-import "./match.css"
+import Axios from "axios";
+import { useState, useEffect } from "react";
+import "./match.css";
 
-
-const Match = ({_id, homeTeam, awayTeam, homeTeamScore, awayTeamScore, status, startTime}) => { 
+const Match = ({
+  _id,
+  homeTeam,
+  awayTeam,
+  homeTeamScore,
+  awayTeamScore,
+  status,
+  startTime,
+}) => {
   const [stat, setStat] = useState(status);
   const [homeScore, setHomeScore] = useState(homeTeamScore);
   const [awayScore, setAwayScore] = useState(awayTeamScore);
@@ -13,92 +20,119 @@ const Match = ({_id, homeTeam, awayTeam, homeTeamScore, awayTeamScore, status, s
   const [aTeam, setaTeam] = useState(awayTeam);
   const [date, setDate] = useState(startTime.slice(0, 10));
   const [time, setTime] = useState(startTime.slice(11, 16));
-  
-  const toggle = () => {
-    if (status === 'soon') {
-      status = "live";
-      Axios.patch(`http://localhost:3003/matches/${_id}`, {
-        "isActive": true,
-        "startTime": new Date()
-      }).then(res => {
-        console.log(res);
-        setStat("live");
-        setHomeScore("0");
-        setAwayScore("0");
-      }).catch(err => console.log(err))
-    };
-  }  
 
-    const handleDelete = () => {
-      Axios.delete(`http://localhost:3003/matches/${_id}`)
-        .then(res => console.log(res))
+  const toggle = () => {
+    if (status === "soon") {
+      status = "live";
+      Axios.patch(`http://localhost:3006/matches/${_id}`, {
+        isActive: true,
+        startTime: new Date(),
+      })
+        .then(res => {
+          console.log(res);
+          setStat("live");
+          setHomeScore("0");
+          setAwayScore("0");
+        })
         .catch(err => console.log(err));
-      window.location.reload(false);
     }
-  
+  };
+
+  const handleDelete = () => {
+    Axios.delete(`http://localhost:3006/matches/${_id}`)
+      .then(res => console.log(res))
+      .catch(err => console.log(err));
+    window.location.reload(false);
+  };
+
   const hanldeEdit = () => {
     setEditMode(true);
-  }
+  };
 
   const submitEdit = () => {
-    const dateTime = date + 'T' + time + ":00.000Z";
+    const dateTime = date + "T" + time + ":00.000Z";
     console.log(dateTime);
-    Axios.patch(`http://localhost:3003/matches/${_id}`, {
-      "homeTeam": hTeam,
-      "awayTeam": aTeam,
-      "startTime": dateTime
-    })
-  }
+    Axios.patch(`http://localhost:3006/matches/${_id}`, {
+      homeTeam: hTeam,
+      awayTeam: aTeam,
+      startTime: dateTime,
+    });
+  };
 
-    return (
-      <div className="match">
+  return (
+    <div className="match">
       <div className="teams">
-          <p className="homeTeam">{ hTeam }</p>
-          <p className="homeTeamScore">{
-            stat === "soon" ? "-" :
-            homeScore}</p>  
-          <span className="status">{ stat }</span>
-          <p className="awayTeamScore">{
-            stat === "soon" ? "-" :
-            awayScore}</p>  
-          <p className="awayTeam">{ aTeam }</p>
+        <p className="homeTeam">{hTeam}</p>
+        <p className="homeTeamScore">{stat === "soon" ? "-" : homeScore}</p>
+        <span className="status">{stat}</span>
+        <p className="awayTeamScore">{stat === "soon" ? "-" : awayScore}</p>
+        <p className="awayTeam">{aTeam}</p>
       </div>
       <div className="timeInfo">
-          <p className="date">{ date }</p>
-          <p className="startTime" >{ time }</p>
-          <button className="btn" onClick={toggle} >{
-            stat === "soon" ? "Start" : "Stop" 
-          }</button>
-        </div>
-        <button className="btn" onClick={handleDelete} >Delete</button>
-        <button className="btn" onClick={hanldeEdit}>Edit</button>
+        <p className="date">{date}</p>
+        <p className="startTime">{time}</p>
+        <button className="btn" onClick={toggle}>
+          {stat === "soon" ? "Start" : "Stop"}
+        </button>
+      </div>
+      <button className="btn" onClick={handleDelete}>
+        Delete
+      </button>
+      <button className="btn" onClick={hanldeEdit}>
+        Edit
+      </button>
 
-        {editMode ?
-          <div>
-            <label>Home Team</label>
-            <input type="text" name="homeTeam" onChange={(e) => {
+      {editMode ? (
+        <div>
+          <label>Home Team</label>
+          <input
+            type="text"
+            name="homeTeam"
+            onChange={e => {
               sethTeam(e.target.value);
-            }} id="" />
-            
-            <label>Away Team</label>
-            <input type="text" name="awayTeam" onChange={(e) => {
+            }}
+            id=""
+          />
+
+          <label>Away Team</label>
+          <input
+            type="text"
+            name="awayTeam"
+            onChange={e => {
               setaTeam(e.target.value);
-            }} id="" />
+            }}
+            id=""
+          />
 
-            <label>Date</label>
-            <input type="text" name="date" placeholder="2020-01-01" onChange={(e) => {
+          <label>Date</label>
+          <input
+            type="text"
+            name="date"
+            placeholder="2020-01-01"
+            onChange={e => {
               setDate(e.target.value);
-            }} id="" />
+            }}
+            id=""
+          />
 
-            <label>Time</label>
-            <input type="text" name="time" placeholder="00:00" onChange={(e) => {
+          <label>Time</label>
+          <input
+            type="text"
+            name="time"
+            placeholder="00:00"
+            onChange={e => {
               setTime(e.target.value);
-            }} id="" />
+            }}
+            id=""
+          />
 
-            <button className="btn" onClick={submitEdit} >OK</button>
-          </div>
-          : <div></div>}
-
+          <button className="btn" onClick={submitEdit}>
+            OK
+          </button>
+        </div>
+      ) : (
+        <div></div>
+      )}
     </div>
   );
 };
--- src/Table.js
+++ src/Table.js
@@ -2,7 +2,7 @@ import React from "react";
 import Match from "./Match";
 import { useState, useEffect } from "react";
 import Axios from "axios";
-import './match.css'
+import "./match.css";
 
 const Table = () => {
   const [data, setData] = useState();
@@ -18,7 +18,7 @@ const Table = () => {
   const [teamStatus, setTeamStatus] = useState();
   const [team, setTeam] = useState();
   useEffect(() => {
-    Axios.get("http://localhost:3003/matches")
+    Axios.get("http://localhost:3006/matches")
       .then(res => {
         setData(res.data.data);
       })
@@ -29,19 +29,45 @@ const Table = () => {
   if (data) {
     const dates = new Set();
 
-    data.map(({ _id, homeTeam, awayTeam, homeTeamScore, awayTeamScore, status, startTime }) => {
-      let date = startTime.slice(0,10)
-      dates.add(date);
-    });
+    data.map(
+      ({
+        _id,
+        homeTeam,
+        awayTeam,
+        homeTeamScore,
+        awayTeamScore,
+        status,
+        startTime,
+      }) => {
+        let date = startTime.slice(0, 10);
+        dates.add(date);
+      }
+    );
 
-
-
-    const matches = data.map(({ _id, homeTeam, awayTeam, homeTeamScore, awayTeamScore, status, startTime }) => {
-      return <Match key={_id} _id={_id} homeTeam={homeTeam} awayTeam={awayTeam}
-        homeTeamScore={homeTeamScore}
-        awayTeamScore={awayTeamScore}
-        startTime={startTime} status={status} />;
-    });
+    const matches = data.map(
+      ({
+        _id,
+        homeTeam,
+        awayTeam,
+        homeTeamScore,
+        awayTeamScore,
+        status,
+        startTime,
+      }) => {
+        return (
+          <Match
+            key={_id}
+            _id={_id}
+            homeTeam={homeTeam}
+            awayTeam={awayTeam}
+            homeTeamScore={homeTeamScore}
+            awayTeamScore={awayTeamScore}
+            startTime={startTime}
+            status={status}
+          />
+        );
+      }
+    );
 
     const datesArray = [];
     for (let value of dates) datesArray.push(value);
@@ -49,121 +75,191 @@ const Table = () => {
     // const groups = [];
 
     const submitNew = () => {
-      const dateTime = date + 'T' + time + ":00.000Z";
-      const endtime = date + 'T' + endTime + ":00.000Z";
-    console.log(dateTime);
-    Axios.post(`http://localhost:3003/matches/`, {
-      "homeTeam": hTeam,
-      "awayTeam": aTeam,
-      "startTime": dateTime,
-      "endTime": endtime,
-      "homeTeamScore": 0,
-      "awayTeamScore": 0,
-      "duration": 90,
-      "isActive": false,
-      "league": "Egyptian league"
-    })
-      setNewMode(false);  
+      const dateTime = date + "T" + time + ":00.000Z";
+      const endtime = date + "T" + endTime + ":00.000Z";
+      console.log(dateTime);
+      Axios.post(`http://localhost:3006/matches/`, {
+        homeTeam: hTeam,
+        awayTeam: aTeam,
+        startTime: dateTime,
+        endTime: endtime,
+        homeTeamScore: 0,
+        awayTeamScore: 0,
+        duration: 90,
+        isActive: false,
+        league: "Egyptian league",
+      });
+      setNewMode(false);
       getAllMatches();
-    }
-
+    };
 
     const filterHandle = () => {
       const fromDate = from + "T00:00:00.000Z";
       const toDate = to + "T00:00:00.000Z";
       const teamstat = teamStatus + "Team";
-      Axios.get(`http://localhost:3003/matches?${teamstat}=${team}`, {
+      Axios.get(`http://localhost:3006/matches?${teamstat}=${team}`, {
         params: {
           "startTime[gte]": fromDate,
           "endTime[lte]": toDate,
-        }
+        },
       })
-        .then(res => { setData(res.data.data) })
+        .then(res => {
+          setData(res.data.data);
+        })
         .catch(err => console.log(err));
-      
-    }
+    };
 
     const getAllMatches = () => {
-      Axios.get("http://localhost:3003/matches")
-      .then(res => {
-        setData(res.data.data);
-      })
-      .catch(err => console.log(err));
-    } 
+      Axios.get("http://localhost:3006/matches")
+        .then(res => {
+          setData(res.data.data);
+        })
+        .catch(err => console.log(err));
+    };
 
     return (
       <div>
-        <button className="btn" onClick={getAllMatches} >All Matches</button>
-        <button className="btn" onClick={(e) => {
-          setNewMode(true);
-        }}>New Match</button>
-        
-        <button className="btn" onClick={(e) => {
-          setFilterMode(true);
-        }}>Filter Matchs</button>
-        
-        {newMode ?
+        <button className="btn" onClick={getAllMatches}>
+          All Matches
+        </button>
+        <button
+          className="btn"
+          onClick={e => {
+            setNewMode(true);
+          }}
+        >
+          New Match
+        </button>
+
+        <button
+          className="btn"
+          onClick={e => {
+            setFilterMode(true);
+          }}
+        >
+          Filter Matchs
+        </button>
+
+        {newMode ? (
           <div>
             <label>Home Team</label>
-            <input type="text" name="homeTeam" onChange={(e) => {
-              sethTeam(e.target.value);
-            }} id="" />
-            
+            <input
+              type="text"
+              name="homeTeam"
+              onChange={e => {
+                sethTeam(e.target.value);
+              }}
+              id=""
+            />
+
             <label>Away Team</label>
-            <input type="text" name="awayTeam" onChange={(e) => {
-              setaTeam(e.target.value);
-            }} id="" />
+            <input
+              type="text"
+              name="awayTeam"
+              onChange={e => {
+                setaTeam(e.target.value);
+              }}
+              id=""
+            />
 
             <label>Date</label>
-            <input type="text" name="date" placeholder="2020-01-01" onChange={(e) => {
-              setDate(e.target.value);
-            }} id="" />
+            <input
+              type="text"
+              name="date"
+              placeholder="2020-01-01"
+              onChange={e => {
+                setDate(e.target.value);
+              }}
+              id=""
+            />
 
             <label>Time</label>
-            <input type="text" name="time" placeholder="00:00" onChange={(e) => {
-              setTime(e.target.value);
-            }} id="" />
+            <input
+              type="text"
+              name="time"
+              placeholder="00:00"
+              onChange={e => {
+                setTime(e.target.value);
+              }}
+              id=""
+            />
 
             <label>Finish Time</label>
-            <input type="text" name="time" placeholder="00:00" onChange={(e) => {
-              setEndTime(e.target.value);
-            }} id="" />
+            <input
+              type="text"
+              name="time"
+              placeholder="00:00"
+              onChange={e => {
+                setEndTime(e.target.value);
+              }}
+              id=""
+            />
 
-            <button className="btn" onClick={submitNew} >OK</button>
+            <button className="btn" onClick={submitNew}>
+              OK
+            </button>
           </div>
-          : <div></div>}
-        {filterMode ?
+        ) : (
+          <div></div>
+        )}
+        {filterMode ? (
           <div>
-
             <label>From Date</label>
-            <input type="text" name="date" placeholder="2020-01-01" onChange={(e) => {
-              setFrom(e.target.value);
-            }} id="" />
+            <input
+              type="text"
+              name="date"
+              placeholder="2020-01-01"
+              onChange={e => {
+                setFrom(e.target.value);
+              }}
+              id=""
+            />
 
             <label>To Date</label>
-            <input type="text" name="date" placeholder="2020-01-01" onChange={(e) => {
-              setTo(e.target.value);
-            }} id="" />
+            <input
+              type="text"
+              name="date"
+              placeholder="2020-01-01"
+              onChange={e => {
+                setTo(e.target.value);
+              }}
+              id=""
+            />
 
             <label>Team status</label>
-            <input type="text" name="Team status" placeholder="Home/Away" onChange={(e) => {
-              setTeamStatus(e.target.value);
-            }} id="" />
+            <input
+              type="text"
+              name="Team status"
+              placeholder="Home/Away"
+              onChange={e => {
+                setTeamStatus(e.target.value);
+              }}
+              id=""
+            />
 
             <label>Team</label>
-            <input type="text" name="Team status" onChange={(e) => {
-              setTeam(e.target.value);
-            }} id="" />
+            <input
+              type="text"
+              name="Team status"
+              onChange={e => {
+                setTeam(e.target.value);
+              }}
+              id=""
+            />
 
-            <button className="btn" onClick={filterHandle} >OK</button>
+            <button className="btn" onClick={filterHandle}>
+              OK
+            </button>
           </div>
-          : <div></div>}
+        ) : (
+          <div></div>
+        )}
         {matches}
-      </div>);
+      </div>
+    );
   } else {
-    return <div>error to get data</div>
+    return <div>error to get data</div>;
   }
-
 };
 
 export default Table;
